b2d11288783fad2977b8b1a4ca97607e1f90eb6f
Emil Platz <Emilplatz@gmail.com>
2016-04-04 00:47:16 +0200

    Add divide and conquer matrix alg

--- src/MatrixMult/daq_matrix_multiplication_major_layout.cpp
+++ src/MatrixMult/daq_matrix_multiplication_major_layout.cpp
@@ -14,56 +14,51 @@ int** MatrixA;
 int** MatrixB;
 int** MatrixC;
 
-int** matrix_add(int** ma, int** mb, int nbeg, int nend, int mbeg, int mend) {
+
+/*int** matrix_add(int** ma, int** mb, int nbeg, int nend, int mbeg, int mend) {
   for (int i=nbeg; i<nend; i++) {
     for (int j=mbeg; j<mend; j++){
       ma[i][j] += mb[i][j];
-      cout << ma[i][j] << " ";
     }
-    cout << endl;
   }
   return ma;
-}
+}*/
 
-int** rec_matrix_mult(int** ma, int** mb, int** mc, int nbeg, int nend, int mbeg, int mend, int pbeg, int pend) {
-  if (nend-nbeg == 1 && mend-mbeg == 1 && pend-pbeg == 1) {
-    mc[nbeg][pbeg] += ma[nbeg][mbeg]*mb[mbeg][pbeg];
-
-  } else if (nend-nbeg >= mend-mbeg && nend-nbeg >= pend-pbeg) {
-    int nmid = nbeg+(nend-nbeg)/2;
-
-    mc = matrix_add(mc, rec_matrix_mult(ma, mb, mc, nbeg, nmid, mbeg, mend, pbeg, pend), nbeg, nmid, pbeg, pend);
-    mc = matrix_add(mc, rec_matrix_mult(ma, mb, mc, nmid, nend, mbeg, mend, pbeg, pend), nmid, nend, pbeg, pend);
-  } else if (pend-pbeg >= mend-mbeg && pend-pbeg >= nend-nbeg) {
-    int pmid = pbeg+(pend-pbeg)/2;
-
-    mc = matrix_add(mc, rec_matrix_mult(ma, mb, mc, nbeg, nend, mbeg, mend, pbeg, pmid), nbeg, nend, pbeg, pmid);
-    mc = matrix_add(mc, rec_matrix_mult(ma, mb, mc, nbeg, nend, mbeg, mend, pmid, pend), nbeg, nend, pmid, pend);
-  } else if (mend-mbeg >= nend-nbeg && mend-mbeg >= pend-pbeg) {
-    int mmid = mbeg+(mend-mbeg)/2;
-
-    mc = matrix_add(mc, rec_matrix_mult(ma, mb, mc, nbeg, nend, mbeg, mmid, pbeg, pend), nbeg, nend, pbeg, pend);
-    mc = matrix_add(mc, rec_matrix_mult(ma, mb, mc, nbeg, nend, mmid, mend, pbeg, pend), nbeg, nend, pbeg, pend);
+int** matrix_mult(int** ma, int** mb, int** mc, int nbeg, int nend, int mbeg, int mend, int pbeg, int pend) {
+  for (int n=nbeg; n<nend;n++) {
+    for (int j=pbeg; j<pend; j++) {
+      int rescell = 0;
+      for (int i=mbeg; i<mend; i++) {
+        rescell += ma[n][i]*mb[j][i];
+      }
+      mc[n][j] += rescell;
+    }
   }
+
   return mc;
 }
 
-int** matrix_mult(int** ma, int** mb) {
-  // MatrixA's rækker
-  int** res = new int*[MatrixARows];
-  // MatrixB's kolonner
-  for (int n=0; n<MatrixARows;n++) {
-    res[n] = new int[MatrixBColumns];
-    for (int j=0; j<MatrixBColumns; j++) {
-      int rescell = 0;
-      for (int i=0; i<MatrixAColumns; i++) {
-        rescell += MatrixA[n][i]*MatrixB[j][i];
-      }
-      res[n][j] = rescell;
-    }
-  }
+int** rec_matrix_mult(int** ma, int** mb, int** mc, int nbeg, int nend, int mbeg, int mend, int pbeg, int pend) {
+  //if (nend-nbeg == 1 && mend-mbeg == 1 && pend-pbeg == 1) {
+  //  mc[nbeg][pbeg] += ma[nbeg][mbeg]*mb[pbeg][mbeg];
+  if (nend-nbeg < 1000 && mend-mbeg < 1000 && pend-pbeg < 1000) {
+    matrix_mult(ma, mb, mc, nbeg, nend, mbeg, mend, pbeg, pend);
+  } else if (nend-nbeg >= mend-mbeg && nend-nbeg >= pend-pbeg) {
+    int nmid = nbeg+(nend-nbeg)/2;
+    rec_matrix_mult(ma, mb, mc, nbeg, nmid, mbeg, mend, pbeg, pend);
+    rec_matrix_mult(ma, mb, mc, nmid, nend, mbeg, mend, pbeg, pend);
+  } else if (pend-pbeg >= mend-mbeg && pend-pbeg >= nend-nbeg) {
+    int pmid = pbeg+(pend-pbeg)/2;
 
-  return res;
+    rec_matrix_mult(ma, mb, mc, nbeg, nend, mbeg, mend, pbeg, pmid);
+    rec_matrix_mult(ma, mb, mc, nbeg, nend, mbeg, mend, pmid, pend);
+  } else if (mend-mbeg >= nend-nbeg && mend-mbeg >= pend-pbeg) {
+    int mmid = mbeg+(mend-mbeg)/2;
+
+    rec_matrix_mult(ma, mb, mc, nbeg, nend, mbeg, mmid, pbeg, pend);
+    rec_matrix_mult(ma, mb, mc, nbeg, nend, mmid, mend, pbeg, pend);
+  }
+  return mc;
 }
 
 // Plads allokkering. Har MatrixA = new int*[N] og MatrixB = new int*[M] stor betydning?
@@ -94,6 +89,7 @@ void setupMatrices() {
   for (int m = 0; m<MatrixCRows; m++){
     MatrixC[m] = new int[MatrixCColumns];
   }
+
 }
 
 int main(int argc, char *argv[]){
@@ -107,7 +103,14 @@ int main(int argc, char *argv[]){
   clock_t start = clock();
   int** res = rec_matrix_mult(MatrixA, MatrixB, MatrixC, 0, MatrixARows, 0, MatrixAColumns, 0, MatrixBColumns);
 
+  /*for (int m = 0; m<MatrixBColumns; m++){
+    for (int i=0; i<MatrixARows; i++) {
+      cout << res[m][i] << " ";
+    }
+    cout << endl;
+  }*/
+
   double time = (clock() - start) / (double)(CLOCKS_PER_SEC / 1000);
-  cout << res[MatrixARows-1][MatrixBColumns-1] << " " << time << endl;
+  cout << MatrixC[MatrixARows-1][MatrixBColumns-1] << " " << time << endl;
   return 0;
 }
